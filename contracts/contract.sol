dc47eccd453618ef3057c91d13d3f3960facd7a2
SujithStellargoldcoin <somraajsujith@gmail.com>
2020-10-05 23:07:03 +0530

    make it better

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -1,76 +1,121 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity >=0.4.25 <0.7.2;
 
-contract GECS {
-    string public constant name = "GECS";
-    string public constant symbol = "GECS";
-    uint8 public constant decimals = 18;  
+/* SafeMath functions */
 
-
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-    event Transfer(address indexed from, address indexed to, uint tokens);
-
-
-    mapping(address => uint256) balances;
-
-    mapping(address => mapping (address => uint256)) allowed;
+contract SafeMath {
     
-    uint256 totalSupply_;
+  function safeMul(uint256 a, uint256 b) pure internal returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
-    using SafeMath for uint256;
+  function safeDiv(uint256 a, uint256 b) pure internal returns (uint256) {
+    assert(b > 0);
+    uint256 c = a / b;
+    assert(a == b * c + a % b);
+    return c;
+  }
 
+  function safeSub(uint256 a, uint256 b) pure internal returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 
-   constructor(uint256 total) public {  
-	totalSupply_ = total;
-	balances[msg.sender] = totalSupply_;
-    }  
+  function safeAdd(uint256 a, uint256 b) pure internal returns (uint256) {
+    uint256 c = a + b;
+    assert(c>=a && c>=b);
+    return c;
+  } 
 
-    function totalSupply() public view returns (uint256) {
-	return totalSupply_;
-    }
-    
-    function balanceOf(address tokenOwner) public view returns (uint) {
-        return balances[tokenOwner];
-    }
-
-    function transfer(address receiver, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[msg.sender]);
-        balances[msg.sender] = balances[msg.sender].sub(numTokens);
-        balances[receiver] = balances[receiver].add(numTokens);
-        emit Transfer(msg.sender, receiver, numTokens);
-        return true;
-    }
-
-    function approve(address delegate, uint numTokens) public returns (bool) {
-        allowed[msg.sender][delegate] = numTokens;
-        Approval(msg.sender, delegate, numTokens);
-        return true;
-    }
-
-    function allowance(address owner, address delegate) public view returns (uint) {
-        return allowed[owner][delegate];
-    }
-
-    function transferFrom(address owner, address buyer, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[owner]);    
-        require(numTokens <= allowed[owner][msg.sender]);
-    
-        balances[owner] = balances[owner].sub(numTokens);
-        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
-        balances[buyer] = balances[buyer].add(numTokens);
-        Transfer(owner, buyer, numTokens);
-        return true;
-    }
 }
 
-library SafeMath { 
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-      assert(b <= a);
-      return a - b;
+interface IGECS {
+    
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract GECS is SafeMath,IGECS {
+    
+    string public constant name = "Governance Enhanced Commercial System";
+    string public constant symbol = "GECS";
+    uint256 public constant decimals = 0;
+    uint256 public override totalSupply = 0;
+    address payable public owner;
+    uint256 private password;
+    
+    constructor() public{
+        uint256 initalSupply =1000000;
+        owner = msg.sender;
+        balanceOf[msg.sender]=initalSupply;
+        totalSupply+=initalSupply;
+        emit Transfer(address(0), owner, initalSupply);
+     }
+
+
+    mapping (address => uint256) public override balanceOf;
+    mapping(address => mapping(address => uint)) allowed;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+
+    
+    function transfer(address _reciever, uint256 _value) public override returns (bool){
+         require(balanceOf[msg.sender]>_value);
+         balanceOf[msg.sender] = SafeMath.safeSub(balanceOf[msg.sender],_value);
+         balanceOf[_reciever] = SafeMath.safeAdd(balanceOf[_reciever],_value);
+         emit Transfer(msg.sender,_reciever,_value);
+         return true;
     }
     
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-      uint256 c = a + b;
-      assert(c >= a);
-      return c;
-    }
+     function transferFrom( address _from, address _to, uint256 _amount )public override returns (bool) {
+     require( _to != address(0));
+     require(balanceOf[_from] >= _amount && allowed[_from][msg.sender] >= _amount && _amount >= 0);
+     balanceOf[_from] = SafeMath.safeSub(balanceOf[_from],_amount);
+     allowed[_from][msg.sender] = SafeMath.safeSub(allowed[_from][msg.sender],_amount);
+     balanceOf[_to] = SafeMath.safeAdd(balanceOf[_to],_amount);
+     emit Transfer(_from, _to, _amount);
+     return true;
+     }
+    
+    function approve(address _spender, uint256 _amount)public override returns (bool) {
+         require( _spender != address(0));
+         allowed[msg.sender][_spender] = _amount;
+         emit  Approval(msg.sender, _spender, _amount);
+         return true;
+     }
+     
+     function reverseApprove(address _spender, uint256 _amount) public returns (bool){
+        require( _spender != address(0));
+        if(SafeMath.safeSub(allowed[msg.sender][_spender],_amount) >= 0){
+        allowed[msg.sender][_spender] = SafeMath.safeSub(allowed[msg.sender][_spender],_amount);
+        emit  Approval(msg.sender, _spender, SafeMath.safeSub(allowed[msg.sender][_spender],_amount));
+        return true;
+        }
+        return false;
+     }
+     
+     
+     function allowance(address _owner, address _spender)public view override returns (uint256 remaining) {
+         require( _owner != address(0) && _spender != address(0));
+         return allowed[_owner][_spender];
+     }
+    
 }
